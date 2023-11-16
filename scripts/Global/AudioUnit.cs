c4168ba6e89c9cb537f3130527b9f1ed42c6e93e
Lisery <1143765064@qq.com>
2023-11-16 12:16:41 +0800

    delete remain cs file

--- scripts/Global/AudioUnit.cs
+++
@@ -1,9 +0,0 @@
-using Godot;
-
-namespace W0rld.scripts.Global;
-
-public partial class AudioUnit : Resource
-{
-    [Export] public AudioStream audio;
-    [Export] public float velocity;
-}
--- scripts/Objs/Abstract/UnMovableObj.cs
+++
@@ -1,9 +0,0 @@
-using Godot;
-using W0rld.scripts.Objs.Interfaces;
-
-namespace W0rld.scripts.Objs.Abstract;
-
-public partial class UnMovableObj : StaticBody2D, IObj
-{
-    public string Information { get; set; }
-}
